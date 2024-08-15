4834399a6a49599e66446d045bcd710d3cf0cf95
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-08-15 21:36:43 +0200

    code improvements in general

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const bigContainer = document.getElementById('bigContainer');
 
 open.addEventListener('click', () => {
     bigContainer.classList.toggle('showmenuNav')
-    
+    // open.style.display = 'none';
+    // close.style.display = 'block';
 })
 
 
 close.addEventListener('click', () => {
     bigContainer.classList.toggle('showmenuNav')
+    // open.style.display = 'block';
+    // close.style.display = 'none';
 })
